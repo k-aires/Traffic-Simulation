e8b46e3ca35eb7abc543aa0f40f81c92193a8799
A.M.Pixels <9497046+ArthurPixels@users.noreply.github.com>
2018-05-11 18:07:34 -0300

    DEUS VULT!!!

--- src/constants.hpp
+++ src/constants.hpp
@@ -12,16 +12,24 @@ class Constants {
    int N2sul_sumidouro_v = 11, N2sul_sumidouro_s = 500;
 
    int S1norte_fonte_f = 30, S1norte_fonte_var = 7;
-   int S1norte_fonte_v = , S1norte_fonte_s = 500;
-   int S1norte_sumidouro_v = , S1norte_sumidouro_s = ;
+   int S1norte_fonte_v = 17, S1norte_fonte_s = 500;
+   int S1norte_sumidouro_v = 17, S1norte_sumidouro_s = 500;
 
-   int N2norte_fonte_f, N2norte_fonte_var;
-   int N2norte_fonte_v, N2norte_fonte_s = ;
-   int N2norte_sumidouro_v, N2norte_sumidouro_s = 500;
+   int S2norte_fonte_f = 60, S2norte_fonte_var = 15;
+   int S2norte_fonte_v = 11, S2norte_fonte_s = 500;
+   int S2norte_sumidouro_v = 11, S2norte_sumidouro_s = 500;
    //80/3.6 = 22
    //30/3.6 = 8
+   int Oeste_fonte_f = 10, Oeste_fonte_var = 2;
+   int Oeste_fonte_v = 22, Oeste_fonte_s = 2000;
+   int Oeste_sumidouro_v = 22, Oeste_sumidouro_s = 2000;
 
+   int Centro_fonte_v = 17, Centro_fonte_s = 300;
+   int Centro_sumidouro_v = 17, Centro_sumidouro_s = 300;
+
+   int Leste_fonte_f = 10, Leste_fonte_var = 2;
+   int Leste_fonte_v = 17, Leste_fonte_s = 400;
+   int Leste_sumidouro_v = 17, Leste_sumidouro_s = 400;
 }
-
 
 #endif

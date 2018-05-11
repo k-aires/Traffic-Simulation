990e9708237e8269e62ce3120799e459a1600156
Helena <lenakaires@gmail.com>
2018-05-11 18:54:35 -0300

    Something, I guess

--- src/estado/police.hpp
+++ src/estado/police.hpp
@@ -4,6 +4,8 @@
 #ifndef POLICE_HPP
 #define POLICE_HPP
 
+#include "time.hpp"
+
 //! Classe Polícia
 /* É a classe responsável por criar e destruir carros quando eles entram na/saem da simulação.
  */
@@ -12,12 +14,27 @@ class Police {
      Police();
      Police(structures::ArrayList<Road> creators, structures::ArrayList<Road> destructors);
 
-     void create_car();
-     void delete_car();
+     void blitz(Time now);
+
+     void create_car(Road road);
+     void delete_car(Road road);
 
  private:
      structures::ArrayList<Road> creators_;
+     structures::ArrayList<std::size_t> ctimes_;
      structures::ArrayList<Road> destructors_;
+     Time last_time_;
 };
+
+Police::Police(structures::ArrayList<Road> creators, structures::ArrayList<Road> destructors) : creators_{creators}, destructors_{destructors}, last_time_{Time()} {
+    ctimes_ = ArrayList<std::size_t>(creators.max_size());
+    srand(0);
+    for (auto i = 1u; i < ctimes_.max_size()+1; i++) {
+        ctimes_.push_back(rand()%10+rand()%10+i);
+    }
+}
+
+void Police::blitz(Time now) {
+}
 
 #endif

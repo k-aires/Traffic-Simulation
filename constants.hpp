13dd2068cee7b46f32cd66c46847207350c21565
A.M.Pixels <9497046+ArthurPixels@users.noreply.github.com>
2018-05-11 17:50:16 -0300

    Create constants.hpp

---
+++ constants.hpp
@@ -0,0 +1,27 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+class Constants {
+ public:
+   int N1sul_fonte_f = 20, N1sul_fonte_var = 5;
+   int N1sul_fonte_v = 17, N1sul_fonte_s = 500;
+   int N1sul_sumidouro_v = 17, N1sul_sumidouro_s = 500;
+
+   int N2sul_fonte_f = 20, N2sul_fonte_var = 5;
+   int N2sul_fonte_v = 11, N2sul_fonte_s 500;
+   int N2sul_sumidouro_v = 11, N2sul_sumidouro_s = 500;
+
+   int S1norte_fonte_f = 30, S1norte_fonte_var = 7;
+   int S1norte_fonte_v = , S1norte_fonte_s = 500;
+   int S1norte_sumidouro_v = , S1norte_sumidouro_s = ;
+
+   int N2norte_fonte_f, N2norte_fonte_var;
+   int N2norte_fonte_v, N2norte_fonte_s = ;
+   int N2norte_sumidouro_v, N2norte_sumidouro_s = 500;
+   //80/3.6 = 22
+   //30/3.6 = 8
+
+}
+
+
+#endif
